747e8a4419bac84e4eb36802704efded7a2f3833
sebastian bascuñan <sebastianbascu.vega@gmail.com>
2017-04-07 17:48:12 -0300

    archivo main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+/* LOOPS
+
+for (var i = 0; i < 10; i++) {
+  console.log (i)
+}
+
+// WHILE
+
+var i = 0
+while(i < 10) {
+  console.log (i + " es menor que 10!")
+  i++
+}
+*/
+
+var vie = "viernes"
+var sab = "sabado"
+var dm = "domingo"
+var diasdelasemana = ["lunes" , "martes" , "miercoles", "jueves" , vie, sab, dm]
+
+for (var i = 0; i < diasdelasemana.length; i++) {
+  console.log(diasdelasemana [i])
+}
